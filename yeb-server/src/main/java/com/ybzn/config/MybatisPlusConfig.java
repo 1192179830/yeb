dcfe9de25f2e2cf1dcb0e85b0c19400ebe4e903d
LiMengJie <1192179830@qq.com>
2021-02-06 13:45:56 +0800

    3

--- yeb-server/src/main/java/com/ybzn/config/MybatisPlusConfig.java
+++ yeb-server/src/main/java/com/ybzn/config/MybatisPlusConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * MybatisPlus自带分页插件
  * @author Hugo
  * @time 2021/2/2
  */
